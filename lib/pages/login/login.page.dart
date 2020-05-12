115420c4c4d78e2d01b918611fbd493a73e8193c
JayYu <jcyu56622@gmail.com>
2020-05-12 20:48:06 +0900

    Rive Teddy 로그인 반영^^

--- lib/pages/login/login.page.dart
+++ lib/pages/login/login.page.dart
@@ -7,6 +7,11 @@ import 'package:clientf/services/app.space.dart';
 import 'package:clientf/widgets/app.drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flare_flutter/flare_actor.dart';
+import 'package:flutter/rendering.dart';
+import 'package:clientf/pages/login/signin_button.dart';
+import 'package:clientf/pages/login/teddy_controller.dart';
+import 'package:clientf/pages/login/tracking_text_input.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -16,6 +21,7 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  TeddyController _teddyController;
 
   /// Gets user registration data from the form
   /// TODO - form validation
@@ -30,51 +36,109 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   @override
+  initState() {
+    _teddyController = TeddyController();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    EdgeInsets devicePadding = MediaQuery.of(context).padding;
+
     return Scaffold(
       appBar: AppBar(
         title: T('login page title'),
       ),
       endDrawer: AppDrawer(),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.all(AppSpace.space),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+      body: Container(
+          child: Stack(
             children: <Widget>[
-              TextField(
-                controller: _emailController,
-                onSubmitted: (text) {},
-                decoration: InputDecoration(
-                  hintText: t('input email'),
-                ),
-              ),
-              AppSpace.halfBox,
-              TextField(
-                controller: _passwordController,
-                onSubmitted: (text) {},
-                decoration: InputDecoration(
-                  hintText: t('input password'),
-                ),
-              ),
-              AppSpace.halfBox,
-              RaisedButton(
-                onPressed: () async {
-                  final data = getFormData();
+              Positioned.fill(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      // Box decoration takes a gradient
+                      gradient: LinearGradient(
+                        // Where the linear gradient begins and ends
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        // Add one stop for each color. Stops should increase from 0 to 1
+                        stops: [0.0, 1.0],
+                        colors: [
+                          Color.fromRGBO(170, 207, 211, 1.0),
+                          Color.fromRGBO(93, 142, 155, 1.0),
+                        ],
+                      ),
+                    ),
+                  )),
+              Positioned.fill(
+                child: SingleChildScrollView(
+                    padding: EdgeInsets.only(
+                        left: 20.0, right: 20.0, top: devicePadding.top + 50.0),
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                              height: 200,
+                              padding: const EdgeInsets.only(left: 30.0, right:30.0),
+                              child: FlareActor(
+                                "assets/Teddy.flr",
+                                shouldClip: false,
+                                alignment: Alignment.bottomCenter,
+                                fit: BoxFit.contain,
+                                controller: _teddyController,
+                              )),
+                          Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(25.0))),
+                              child: Padding(
+                                padding: const EdgeInsets.all(30.0),
+                                child: Form(
+                                    child: Column(
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                        TrackingTextInput(
+                                            label: "이메일주소",
+                                            hint: "이메일주소를 입력하세요.",
+                                            onCaretMoved: (Offset caret) {
+                                              _teddyController.lookAt(caret);
+                                            },
+                                            onTextChanged: (String value) {
+                                              _teddyController.setEmail(value);
+                                            },
+                                            ),
 
-                  try {
-                    await app.login(data['email'], data['password']);
-                    AppRouter.open(context, AppRoutes.home);
-                  } catch (e) {
-                    AppService.alert(null, t(e));
-                  }
-                },
-                child: T('login submit'),
+                                        TrackingTextInput(
+                                          label: "비밀번호",
+                                          hint: "비밀번호를 넣으세요, 테디가 안볼께요.",
+                                          isObscured: true,
+                                          onCaretMoved: (Offset caret) {
+                                            _teddyController.coverEyes(caret != null);
+                                            _teddyController.lookAt(null);
+                                          },
+                                          onTextChanged: (String value) {
+                                            _teddyController.setPassword(value);
+                                          },
+                                        ),
+                                        SigninButton(
+                                            child: Text("로그인",
+                                                style: TextStyle(
+                                                    fontSize: 16,
+                                                    color: Colors.white)),
+                                            onPressed: () {
+                                              _teddyController.submitPassword();
+                                            })
+                                      ],
+                                    )),
+                              )),
+                        ])),
               ),
             ],
-          ),
-        ),
-      ),
+          )),
+
     );
   }
 }
